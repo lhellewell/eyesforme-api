2a80cefa098d70aafefc77cc6935ce10a7f61b6f
Lewis-Hellewell <imlewishellewell@gmail.com>
2023-03-10 12:54:09 +0800

    feat: submission end points

---
+++ Controllers/ValueController.cs
@@ -0,0 +1,21 @@
+﻿using EyesApiJwt.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EyesApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        
+        [HttpPost("submit")]
+        public ActionResult<Submission>? Submit(Submission submission, string token)
+        {
+
+            return null;
+        }
+
+    }
+}
--- Models/Submission.cs
+++ Models/Submission.cs
@@ -7,7 +7,6 @@ namespace EyesApiJwt.Models
         public int SubmissionId { get; set; }
         public DateTime TimeSubmitted { get; set; }
         public string UserId { get; set; } = null!;
-        public User User { get; set; } = null!;
         public byte[] Image { get; set; } = null!;
         public string Task { get; set; } = null!;
         public string? Input { get; set; }
