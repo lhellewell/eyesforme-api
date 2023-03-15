929a13779aae8544e755f77e3d71496a20c0f8df
Lewis-Hellewell <imlewishellewell@gmail.com>
2023-03-15 16:57:41 +0800

    refactor: file deletions

--- Models/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EyesApiJwt.Models
-{
-    public class UserDto
-    {
-        public required string UserId { get; set; }
-        public required string Password { get; set; }
-    }
-}
